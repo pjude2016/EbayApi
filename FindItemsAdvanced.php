612217183645e9e31bc5a788922c1bed9462fd22
Pius <piusjude@Pius.local>
2018-02-11 21:40:30 +0000

    adding into database5

--- FindItemsAdvanced.php
+++ FindItemsAdvanced.php
@@ -360,21 +360,20 @@ if(isset($_POST['Query']))
         //$endTime = strtotime($item->listingInfo->endTime);   // returns Epoch seconds
         $endTime = $item->listingInfo->endTime;
         $startTime = $item->listingInfo->startTime;
-        $sqlItemSellingStatus = sprintf("%01.2f", $item->sellingStatus->convertedCurrentPrice);
-        $sqlItemShippingInfo = sprintf("%01.2f", $item->shippingInfo->shippingServiceCost);
-        $sqlItemTitle = $item->title;
-        //$res = odbc_exec($conn, $sql);
         $sql = "INSERT INTO Product_Searches (title, price, serviceCost)
         VALUES ('$sqlItemTitle','$sqlItemSellingStatus','$sqlItemShippingInfo' )";
         $res = odbc_exec($conn, $sql);
-        if (!$res) {
-          print("Table creation failed with error:\n");
-          print(odbc_error($conn).": ".odbc_errormsg($conn)."\n");
-        } else {
+          if (!$res) {
+            print("Table creation failed with error:\n");
+            print(odbc_error($conn).": ".odbc_errormsg($conn)."\n");
+          } else {
             print("Table fyi_links created.\n");
           }
-  // Free the connection
-  @odbc_close($conn);
+
+
+          // Free the connection
+
+          @odbc_close($conn);
         $results .= "<tr><td>$count</td><td><a href=\"$link\"><img src=\"$picURL\"></a></td><td> <a href=\"$link\">$title</a></br></br> $subtitle </br></br> $sellingState </br></br> $bids</br></br> $condition</br></br>$conditionInfo</br></br> </br> $ebayItemId</br></br> $display</br><td >$location</td>"
              .  "<td>$price</td><td>$ship</td><td>$total</td><td>$curr</td><td>$timeLeft</td><td><nobr>$startTime</nobr></td><td><nobr>$endTime</nobr></td></tr>";
             $count++;
