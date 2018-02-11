5fafc81f929aa5c33deea1e3b14f32615f0f7530
Pius <piusjude@Pius.local>
2018-02-11 21:17:20 +0000

    adding into database

--- FindItemsAdvanced.php
+++ FindItemsAdvanced.php
@@ -364,19 +364,18 @@ if(isset($_POST['Query']))
         $sqlItemShippingInfo = $item->shippingInfo->shippingServiceCost;
         $sqlItemTitle = $item->title;
 
-  
+
         $sql = "INSERT INTO Product_Searches (title, price, serviceCost)
         VALUES ('$sqlItemTitle','$sqlItemSellingStatus','$sqlItemShippingInfo' )";
         $res = odbc_exec($conn, $sql);
         if (!$res) {
           print("Table creation failed with error:\n");
           print(odbc_error($conn).": ".odbc_errormsg($conn)."\n");
+        } else {
+          print("Table fyi_links created.\n");
         }
-        else {
-            print("Table fyi_links created.\n");
-          }
-          //Free the connection
-          @odbc_close($conn);
+        // Free the connection
+        @odbc_close($conn);
 
         $results .= "<tr><td>$count</td><td><a href=\"$link\"><img src=\"$picURL\"></a></td><td> <a href=\"$link\">$title</a></br></br> $subtitle </br></br> $sellingState </br></br> $bids</br></br> $condition</br></br>$conditionInfo</br></br> </br> $ebayItemId</br></br> $display</br><td >$location</td>"
              .  "<td>$price</td><td>$ship</td><td>$total</td><td>$curr</td><td>$timeLeft</td><td><nobr>$startTime</nobr></td><td><nobr>$endTime</nobr></td></tr>";
