d00ea7973c596f595c4caa1fe80ae62e8ac6f0de
Pius <piusjude@Pius.local>
2018-02-14 20:55:21 +0000

    return filter5

--- products.php
+++ products.php
@@ -341,7 +341,7 @@ if(isset($_POST['Query']))
          . "&itemFilter(3).name=Condition"
          . "&itemFilter(3).value=$cond"
          . "&itemFilter(4).name=ReturnsAcceptedOnly"
-         . "&itemFilter(4).value=0"
+         . "&itemFilter(4).value=false"
 
 
          . "&aspectFilter(0).aspectName=Display"
@@ -392,7 +392,7 @@ if(isset($_POST['Query']))
          . "&itemFilter(3).name=Condition"
          . "&itemFilter(3).value=$cond"
          . "&itemFilter(4).name=ReturnsAcceptedOnly"
-         . "&itemFilter(4).value=0"
+         . "&itemFilter(4).value=false"
 
 
          . "&aspectFilter(0).aspectName=Display"
