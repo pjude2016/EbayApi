981c2019786c56b651303e9c1eb7e40cec0a22c5
Pius <piusjude@Pius.local>
2018-02-18 16:17:29 +0000

    added filter database connection duplicaton12

--- products.php
+++ products.php
@@ -304,7 +304,8 @@ if(isset($_POST['Query']))
   if(!($max>0)) {
     $rest2 = die("Error: Please select the required filters");
 }
-  $query2 = "SELECT * FROM auction.filters WHERE brand = '$brand' AND min_price = '$min' "
+  //$query2 = "SELECT * FROM auction.filters WHERE brand = '$brand' AND min_price = '$min' AND max_price = '$max' AND display ='$disp' AND condition = '$cond' AND gender = '$gend' AND year_manufacture = '$year' "
+  $query2 = "SELECT * FROM auction.filters WHERE brand = '$brand' "
   $getMatches2= sqlsrv_query($conn, $query2);
   $row2 = sqlsrv_fetch_array($getMatches2, SQLSRV_FETCH_ASSOC);
   if(!$row2){
