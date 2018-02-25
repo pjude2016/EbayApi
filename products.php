36c158ebb49ee6ee584805519c981156ba0febbb
Pius <piusjude@Pius.local>
2018-02-25 18:36:55 +0000

    view count update database7

--- products.php
+++ products.php
@@ -594,13 +594,13 @@ if(isset($_POST['Query']))
           echo "id ";
           echo $id;
           $update_count = $count + 1;
-          //echo $update_count;
+          echo $update_count;
           echo "</br>";
-          // $sql = "UPDATE auction.product_searches SET view_count=$update_count WHERE id=$id";
-          // $getResultsD= sqlsrv_query($conn, $sql);
-          // $rowsAffected = sqlsrv_rows_affected($getResultsD);
-          // if ($getResults == FALSE or $rowsAffected == FALSE)
-          //     die(FormatErrors(sqlsrv_errors()));
+          $sql = "UPDATE auction.product_searches SET view_count=$update_count WHERE id=$id";
+          $getResultsD= sqlsrv_query($conn, $sql);
+          $rowsAffected = sqlsrv_rows_affected($getResultsD);
+          if ($getResults == FALSE or $rowsAffected == FALSE)
+              die(FormatErrors(sqlsrv_errors()));
 
         }
 
