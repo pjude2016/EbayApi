d9ac94c05c9c8d4635cb1c10b5c51f26b19ffb14
Pius <piusjude@Pius.local>
2018-02-27 23:21:35 +0000

    establish database connection7

--- review.php
+++ review.php
@@ -5,15 +5,12 @@ $_SESSION['message'] = '';
 $connectionInfo = array("UID" => "auctora@auctora-server", "pwd" => "arotcua1!", "Database" => "auctoraDB");
 $serverName = "tcp:auctora-server.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-if ($conn){
-  echo "hello";
-}
+
  echo "<br><br><br><br><br>";
 
-echo "Review";
+echo "<h1 align='center'>eBay Watch Search form</h1>";
 echo "</br>";
 $ebayItemId = $_POST['ebayID'];
-echo "Ebay Item ID: ";
 echo $ebayItemId;
 
 
