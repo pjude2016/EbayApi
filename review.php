73906f2617dc2258de849f6cade0ea8266494856
Pius <piusjude@Pius.local>
2018-02-27 23:03:41 +0000

    establish database connection

--- review.php
+++ review.php
@@ -5,6 +5,12 @@
 </body>
 <?php
 session_start();
+$_SESSION['message'] = '';
+$connectionInfo = array("UID" => "auctora@auctora-server", "pwd" => "arotcua1!", "Database" => "auctoraDB");
+$serverName = "tcp:auctora-server.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
 echo "Review";
 echo "</br>";
 $ebayItemId = $_POST['ebayID'];
