54d27d8deeef0c2793aaa70ac4fa989d54ab19fd
Pius <piusjude@eduroam-int-dhcp-97-223-241.ucl.ac.uk>
2018-02-28 13:57:44 +0000

    product id database25

--- review.php
+++ review.php
@@ -216,8 +216,8 @@ echo "</tr>";
 
 
 
-  $tsql2= "INSERT INTO auction.product_reviews (comment, rating, user_id, product_id ) VALUES (?,?,?,?);";
-  $params2 = array($comment,$rating,$currentUserId,$prod_id);
+  $tsql2= "INSERT INTO auction.product_reviews (product_id, user_id, comment, rating) VALUES (?,?,?,?);";
+  $params2 = array($prod_id,$currentUserId,$comment,$rating);
   $getResults2= sqlsrv_query($conn, $tsql2, $params2);
   $rowsAffected2 = sqlsrv_rows_affected($getResults2);
   if ($getResults2 == FALSE or $rowsAffected2 == FALSE){
