37eebc532363ea2bbe014366bca5f12ef925963d
Thomas Yenbamroong <pthomas.y@gmail.com>
2018-02-28 13:04:59 +0000

    1

--- review.php
+++ review.php
@@ -177,6 +177,7 @@ echo "</tr>";
     <div class="col-md-8 container forum-full">
         <form action="" method="post" id="writereviews">
           <textarea placeholder="Write your review..." class="col-md-12 ckeditor" name="reviewBody" rows="8"></textarea>
+          <?php $productid = row['ID']; ?>
           <input type="submit" value="Post" style="background:green;color:white;margin-top:10px;">
         </form>
     </div>
