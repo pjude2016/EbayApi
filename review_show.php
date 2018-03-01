550cabf94cf56aaf48eaf8a40df1ec4ccd78c460
Pius <piusjude@Pius.local>
2018-03-01 21:36:30 +0000

    show product on review_show.php

--- review_show.php
+++ review_show.php
@@ -1,4 +1,63 @@
-<? php ?>
+<?php
+
+session_start();
+$_SESSION['message'] = '';
+$connectionInfo = array("UID" => "auctora@auctora-server", "pwd" => "arotcua1!", "Database" => "auctoraDB");
+$serverName = "tcp:auctora-server.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+ echo "<br><br><br><br><br>";
+
+echo "<h1 align='center'>eBay Watch Review</h1>";
+echo "</br>";
+//ebayItem id from product_searches page
+$ebayItemId = $_POST['ebayID'];
+$current_uid = $_SESSION['user_id'];
+echo "Ebay item id is" . $ebayItemId;
+//user primary key
+$currentId = $_SESSION['userID'];
+
+echo "</br>";
+
+
+
+echo "</br></br>";
+$query = "SELECT * FROM auction.product_searches WHERE ebayID = '$ebayItemId'";
+$getMatches= sqlsrv_query($conn, $query);
+
+$row = sqlsrv_fetch_array($getMatches, SQLSRV_FETCH_ASSOC);
+$count = $row['view_count'];
+//obtain below product primary key id from database
+$prod_id = $row['ID'];
+$_SESSION['productID'] = $row['ID'];
+echo "product1_id ";
+echo $prod_id;
+
+
+echo "
+
+<table border='1' align='center'>
+<tr>
+<th>Image</th>
+<th>Title</th>
+<th>Price</th>
+<th>Service Cost</th>
+<th>ebayID</th>
+</tr>";
+
+
+$product_link = $row['product_link'];
+$title = $row['title'];
+$img_src = $row['image'];
+echo "<tr>";
+echo "<td>" . "<a href=\"$product_link\"><img src=\"$img_src\"></a>" . "</td>";
+echo "<td>" . "<a href=\"$product_link\" target=\"_blank\">$title</a>" . "</td>";
+echo "<td>" . $row['price'] . "</td>";
+echo "<td>" . $row['serviceCost'] . "</td>";
+echo "<td>" . $row['ebayID'] . "</td>";
+echo "</tr>";
+
+?>
 
 
 
