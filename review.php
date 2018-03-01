3a5a9795010bd82abd9904091804e71b174f6bcd
Pius <piusjude@Pius.local>
2018-03-01 00:10:56 +0000

    session for productid3

--- review.php
+++ review.php
@@ -1,91 +1,4 @@
 
-<?php
-session_start();
-$_SESSION['message'] = '';
-$connectionInfo = array("UID" => "auctora@auctora-server", "pwd" => "arotcua1!", "Database" => "auctoraDB");
-$serverName = "tcp:auctora-server.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
- echo "<br><br><br><br><br>";
-
-echo "<h1 align='center'>eBay Watch Review</h1>";
-echo "</br>";
-//ebayItem id from product_searches page
-$ebayItemId = $_POST['ebayID'];
-$current_uid = $_SESSION['user_id'];
-
-//user primary key
-$currentId = $_SESSION['userID'];
-
-echo "</br>";
-
-
-
-echo "</br></br>";
-$query = "SELECT * FROM auction.product_searches WHERE ebayID = '$ebayItemId'";
-$getMatches= sqlsrv_query($conn, $query);
-
-$row = sqlsrv_fetch_array($getMatches, SQLSRV_FETCH_ASSOC);
-$count = $row['view_count'];
-//obtain below product primary key id from database
-$prod_id = $row['ID'];
-echo "product1_id ";
-echo $prod_id;
-
-
-echo "
-
-<table border='1' align='center'>
-<tr>
-<th>Image</th>
-<th>Title</th>
-<th>Price</th>
-<th>Service Cost</th>
-<th>ebayID</th>
-</tr>";
-
-
-$product_link = $row['product_link'];
-$title = $row['title'];
-$img_src = $row['image'];
-echo "<tr>";
-echo "<td>" . "<a href=\"$product_link\"><img src=\"$img_src\"></a>" . "</td>";
-echo "<td>" . "<a href=\"$product_link\" target=\"_blank\">$title</a>" . "</td>";
-echo "<td>" . $row['price'] . "</td>";
-echo "<td>" . $row['serviceCost'] . "</td>";
-echo "<td>" . $row['ebayID'] . "</td>";
-echo "</tr>";
-
-
-
-// $tsql2= "INSERT INTO auction.product_reviews (comment, rating, user_id, product_id ) VALUES (?,?,?,?);";
-// $params2 = array($title,$rating,$currentId,$id);
-// $getResults2= sqlsrv_query($conn, $tsql2, $params2);
-// $rowsAffected2 = sqlsrv_rows_affected($getResults2);
-// if ($getResults2 == FALSE or $rowsAffected2 == FALSE){
-//   die(FormatErrors(sqlsrv_errors()));
-// }
-
-
-// $currentUserId = $_SESSION['userID'];
-//
-// $productid = $row['ID'];
-// $comment = $_POST['reviewBody'];
-// echo "product";
-// echo $productid;
-//
-// $rating =5;
-//
-//
-//
-// $tsql2= "INSERT INTO auction.product_reviews (comment, rating, user_id, product_id ) VALUES (?,?,?,?);";
-// $params2 = array($comment,$rating,$currentUserId,$productid);
-// $getResults2= sqlsrv_query($conn, $tsql2, $params2);
-// $rowsAffected2 = sqlsrv_rows_affected($getResults2);
-// if ($getResults2 == FALSE or $rowsAffected2 == FALSE){
-//   die(FormatErrors(sqlsrv_errors()));
-// }
-?>
 
 
 <!DOCTYPE html>
@@ -204,26 +117,113 @@ echo "</tr>";
         </form>
     </div>
 
+    <?php
+    session_start();
+    $_SESSION['message'] = '';
+    $connectionInfo = array("UID" => "auctora@auctora-server", "pwd" => "arotcua1!", "Database" => "auctoraDB");
+    $serverName = "tcp:auctora-server.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-  <?php
-  $comment = $_POST['reviewBody'];
-  $currentUserId = $_SESSION['userID'];
+     echo "<br><br><br><br><br>";
+
+    echo "<h1 align='center'>eBay Watch Review</h1>";
+    echo "</br>";
+    //ebayItem id from product_searches page
+    $ebayItemId = $_POST['ebayID'];
+    $current_uid = $_SESSION['user_id'];
+
+    //user primary key
+    $currentId = $_SESSION['userID'];
+
+    echo "</br>";
 
 
-  //echo "product";
-  //echo $prod_id;
 
-  $rating =5;
+    echo "</br></br>";
+    $query = "SELECT * FROM auction.product_searches WHERE ebayID = '$ebayItemId'";
+    $getMatches= sqlsrv_query($conn, $query);
 
-  //push review to database
-  $tsql2= "INSERT INTO auction.product_reviews (product_id, user_id, comment, rating) VALUES (?,?,?,?);";
-  $params2 = array($prod_id,$currentUserId,$comment,$rating);
-  $getResults2= sqlsrv_query($conn, $tsql2, $params2);
-  $rowsAffected2 = sqlsrv_rows_affected($getResults2);
-  if ($getResults2 == FALSE or $rowsAffected2 == FALSE){
-    die(FormatErrors(sqlsrv_errors()));
-  }
-  ?>
+    $row = sqlsrv_fetch_array($getMatches, SQLSRV_FETCH_ASSOC);
+    $count = $row['view_count'];
+    //obtain below product primary key id from database
+    $prod_id = $row['ID'];
+    echo "product1_id ";
+    echo $prod_id;
+
+
+    echo "
+
+    <table border='1' align='center'>
+    <tr>
+    <th>Image</th>
+    <th>Title</th>
+    <th>Price</th>
+    <th>Service Cost</th>
+    <th>ebayID</th>
+    </tr>";
+
+
+    $product_link = $row['product_link'];
+    $title = $row['title'];
+    $img_src = $row['image'];
+    echo "<tr>";
+    echo "<td>" . "<a href=\"$product_link\"><img src=\"$img_src\"></a>" . "</td>";
+    echo "<td>" . "<a href=\"$product_link\" target=\"_blank\">$title</a>" . "</td>";
+    echo "<td>" . $row['price'] . "</td>";
+    echo "<td>" . $row['serviceCost'] . "</td>";
+    echo "<td>" . $row['ebayID'] . "</td>";
+    echo "</tr>";
+
+
+
+    // $tsql2= "INSERT INTO auction.product_reviews (comment, rating, user_id, product_id ) VALUES (?,?,?,?);";
+    // $params2 = array($title,$rating,$currentId,$id);
+    // $getResults2= sqlsrv_query($conn, $tsql2, $params2);
+    // $rowsAffected2 = sqlsrv_rows_affected($getResults2);
+    // if ($getResults2 == FALSE or $rowsAffected2 == FALSE){
+    //   die(FormatErrors(sqlsrv_errors()));
+    // }
+
+
+    // $currentUserId = $_SESSION['userID'];
+    //
+    // $productid = $row['ID'];
+    // $comment = $_POST['reviewBody'];
+    // echo "product";
+    // echo $productid;
+    //
+    // $rating =5;
+    //
+    //
+    //
+    // $tsql2= "INSERT INTO auction.product_reviews (comment, rating, user_id, product_id ) VALUES (?,?,?,?);";
+    // $params2 = array($comment,$rating,$currentUserId,$productid);
+    // $getResults2= sqlsrv_query($conn, $tsql2, $params2);
+    // $rowsAffected2 = sqlsrv_rows_affected($getResults2);
+    // if ($getResults2 == FALSE or $rowsAffected2 == FALSE){
+    //   die(FormatErrors(sqlsrv_errors()));
+    // }
+
+    $comment = $_POST['reviewBody'];
+    $currentUserId = $_SESSION['userID'];
+
+
+    //echo "product";
+    //echo $prod_id;
+
+    $rating =5;
+
+    //push review to database
+    $tsql2= "INSERT INTO auction.product_reviews (product_id, user_id, comment, rating) VALUES (?,?,?,?);";
+    $params2 = array($prod_id,$currentUserId,$comment,$rating);
+    $getResults2= sqlsrv_query($conn, $tsql2, $params2);
+    $rowsAffected2 = sqlsrv_rows_affected($getResults2);
+    if ($getResults2 == FALSE or $rowsAffected2 == FALSE){
+      die(FormatErrors(sqlsrv_errors()));
+    }
+    ?>
+
+
 <!-- <section>
 Welcome HERREE <span class="user"><?= $_SESSION['firstname'] ?></span>
 </section> -->
