36e76bf28d1b18314ae7cab2bdc739a8b04689e2
Pius <piusjude@Pius.local>
2018-03-02 12:19:26 +0000

    links to review show  on watchlist.php2

--- watchlist.php
+++ watchlist.php
@@ -57,7 +57,7 @@ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
   // $ebayidval = $row['ebayID'];
   $product_id = $row['ID'];
   $img_src = $row['image'];
-  $ebayID=$row['ebayID']
+  $ebayID = $row['ebayID']
   echo "<tr>";
 
   echo "<td>" . "<a href=\"$product_link\"><img src=\"$img_src\"></a>" . "</td>";
