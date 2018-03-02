1a2908ed94419ac3eb5ad55101bbc5c29f7ff2c2
Pius <piusjude@Pius.local>
2018-03-02 12:27:49 +0000

    links to review show  on watchlist.php4

--- watchlist.php
+++ watchlist.php
@@ -66,7 +66,8 @@ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
   echo "<td>" . $row['serviceCost'] . "</td>";
   echo "<td>" . $row['ebayID'] . "</td>";
   echo "<td>" ;
-  echo " <div style=\"text-align:center\"> <form method=\"POST\" action=\"review.php\" >  <button type=\"submit\" class=\"btn btn-danger\" name=\"ebayID\" value=\"$ebayID\" >Add Your Review</button></form> </div>";
+  echo $ebayID;
+  //echo " <form method=\"POST\" action=\"review.php\" >  <button type=\"submit\" class=\"btn btn-danger\" name=\"ebayID\" value=\"$ebayID\" >Add Your Review</button></form>";
   echo "</td>";
   echo "<td>" . "<form id= \"delete_item\" method=\"post\">  <button type=\"submit\" class=\"btn btn-warning\" name=\"delete_item\" onclick=\"return confirm('Remove item?');\" value=\"$product_id\">Remove Item</button></form> </td>";
   echo "</tr>";
