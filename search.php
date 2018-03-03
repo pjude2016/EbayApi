219195f67406de74fa1c5aab4e8b6b314a03c5be
Pius <piusjude@Pius.local>
2018-03-03 23:50:56 +0000

    new search page3

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ $responseEncoding = 'XML';   // Format of the response
 
 
   // Construct the FindItems call
-  $apicall = "https://api.ebay.com/buy/browse/v1/item_summary/search?epid=182527490"
+  $apicall = "https://api.ebay.com/buy/browse/v1/item_summary/search?q=phone"
   . "&RESPONSE-DATA-FORMAT=$responseEncoding";
   // . "&GLOBAL-ID=EBAY-GB"
   // . "&SECURITY-APPNAME=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424" //replace with your app id
