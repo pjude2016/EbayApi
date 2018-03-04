1a888232d55eefa5aaa5d1bb1791c8fc3c56d044
Pius <piusjude@Pius.local>
2018-03-04 10:34:33 +0000

    new search page29

--- search.php
+++ search.php
@@ -7,12 +7,12 @@ $responseEncoding = 'XML';   // Format of the response
   $apicall = "http://open.api.ebay.com/shopping?"
     //  . "&callname=item_summary/search?q=watch"
       . "&callname=GetSingleItem"
-      . "*responseencoding=XML"
+      . "&responseencoding=XML"
       . "&appid=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424" //replace with your app id
       . "&version=967"
       . "&ItemID=273084369154"
 
 
        $rest = simplexml_load_file($apicall) or die("Error: Please select the required filters");
-       echo $rest;
+       echo $rest->Seller->FeedbackScore;
 ?>
