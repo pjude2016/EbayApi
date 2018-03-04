0b66af304aec22d7dcbdd21c96810940f674849e
Pius <piusjude@Pius.local>
2018-03-04 09:52:50 +0000

    new search page25

--- search.php
+++ search.php
@@ -4,11 +4,11 @@ $responseEncoding = 'JSON';   // Format of the response
 
 
   // Construct the FindItems call
-  $apicall = "https://api.ebay.com/buy/browse/v1/item_summary/search?q=phone"
+  $apicall = "https://api.ebay.com/buy/browse/v2/item_summary/search?q=phone"
     //  . "&callname=item_summary/search?q=watch"
     . "&appid=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424" //replace with your app id
     . "&responseencoding=$responseEncoding"
-    . "&version=967";
+    . "&version=2.0.0";
 
 
 
