18990695f0419e942420b531987c978359519ef7
Pius <piusjude@Pius.local>
2018-03-04 00:01:28 +0000

    new search page8

--- search.php
+++ search.php
@@ -7,10 +7,11 @@ $responseEncoding = 'XML';   // Format of the response
   $apicall = "https://api.ebay.com/buy/browse/v1/item_summary/search?q=phone"
 
   // . "&GLOBAL-ID=EBAY-GB"
-   . "&SECURITY-APPNAME=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424" //replace with your app id
+   . "&SERVICE-VERSION=1.0.0"
+   . "&CONSUMER-ID=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424" //replace with your app id
    . "&RESPONSE-DATA-FORMAT=$responseEncoding";
 
-       // . "&SERVICE-VERSION=1.0.0"
+       //
 
        //
        // . "&RESPONSE-DATA-FORMAT=$responseEncoding";
