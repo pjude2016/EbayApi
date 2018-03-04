e0d63e1634f534646d01850d53512b76c9ef8403
Pius <piusjude@Pius.local>
2018-03-04 09:38:35 +0000

    new search page22

--- search.php
+++ search.php
@@ -4,8 +4,8 @@ $responseEncoding = 'XML';   // Format of the response
 
 
   // Construct the FindItems call
-  $apicall = "https://api.ebay.com/buy/browse/v1/"
-     . "&callname=item_summary/search?q=watch"
+  $apicall = "https://api.ebay.com/buy/browse/v1/item_summary/search?q=phone"
+    //  . "&callname=item_summary/search?q=watch"
     . "&appid=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424" //replace with your app id
     . "&responseencoding=$responseEncoding"
     . "&version=967";
@@ -13,6 +13,6 @@ $responseEncoding = 'XML';   // Format of the response
 
 
        $rest = simplexml_load_file($apicall) or die("Error: Please select the required filters");
-       echo $rest->itemSummaries->price->value;
-       //https://codeshare.io/5z6Z7b
+       echo $rest->price->value;
 ?>
+https://codeshare.io/5z6Z7b
