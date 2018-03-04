7a9a525793ad5a7e2c55e6878db35f684d0af476
Pius <piusjude@Pius.local>
2018-03-04 19:16:18 +0000

    get similar items2

--- similarItems.php
+++ similarItems.php
@@ -11,13 +11,40 @@ $responseEncoding = 'XML';   // Format of the response
    . "&listingType=Chinese";
 
 
-       $rest = simplexml_load_file($apicall) or die("Error: Please select the required filters");
-       echo "hello";
-          foreach($rest->itemRecommendations->item as $item) {
-            $id=$item->itemId;
-            echo "id ";
-            echo $id;
-            echo "</br>";
-          }
+    $rest = simplexml_load_file($apicall) or die("Error: Please select the required filters");
+       echo "
+       <br><br><br><br><br>
+       <h3 align='center'Similar Items on Auction</h3>
 
+       <table border='1' align='center'>
+       <tr>
+       <th>Image</th>
+       <th>Title</th>
+       <th>Price</th>
+       <th>Service Cost</th>
+       <th>ebayID</th>
+       </tr>";
+       foreach($rest->itemRecommendations->item as $item) {
+            $id=$item->itemId;
+            $title=$item->title;
+            $price=$item->currentPrice;
+
+            if ($item->imageURL) {
+              $picURL = $item->imageURL;
+            } else {
+              $picURL = "http://pics.ebaystatic.com/aw/pics/express/icons/iconPlaceholder_96x96.gif";
+            }
+            $image = (string) $picURL;
+            $link  = $item->viewItemURL;
+            $servicecost=$item->shippingCost;
+            echo "<tr>";
+
+            echo "<td>" . "<a href=\"$link\"><img src=\"$picURL\"></a>" . "</td>";
+            echo "<td>" . "<a href=\"$link\">$title</a>" . "</td>";
+            echo "<td>" . $price . "</td>";
+            echo "<td>" . $servicecost . "</td>";
+            echo "<td>" . $id . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
 ?>
