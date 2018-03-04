45793e2045c7e40b27169cbc1980de70afec4aec
Pius <piusjude@Pius.local>
2018-03-04 00:33:22 +0000

    new search page19

--- search.php
+++ search.php
@@ -1,14 +1,14 @@
 <?php
-$endpoint = 'http://svcs.ebay.com/services/search/FindingService/v1';  // URL to call
+//$endpoint = 'http://svcs.ebay.com/services/search/FindingService/v1';  // URL to call
 $responseEncoding = 'XML';   // Format of the response
 
 
   // Construct the FindItems call
-  $apicall = "https://api.ebay.com/buy/browse/v1/"
-    . "&callname=item_summary/search?q=phone"
+  $apicall = "https://api.ebay.com/buy/browse/v1/item_summary/search?q=phone"
+    //  . "&callname=item_summary/search?q=watch"
     . "&appid=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424" //replace with your app id
     . "&responseencoding=$responseEncoding"
-    . "&version=1.0.0";
+    . "&version=967";
 
 
 
