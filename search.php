df29ee42b96994bb867132bef8ed5949547969e6
Pius <piusjude@Pius.local>
2018-03-04 18:55:04 +0000

    new search page50

--- search.php
+++ search.php
@@ -5,10 +5,10 @@ $responseEncoding = 'XML';   // Format of the response
 
   // Construct the getSimilarItems call
   $apicall = "http://svcs.ebay.com/MerchandisingService?OPERATION-NAME=getSimilarItems&SERVICE-NAME=MerchandisingService&SERVICE-VERSION=1.1.0&CONSUMER-ID=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424&RESPONSE-DATA-FORMAT=XML"
-   ."&REST-PAYLOAD"
-   ."&itemId=391989027483"
-   ."&maxResults=3"
-   ."&listingType=Chinese";
+   . "&REST-PAYLOAD"
+   . "&itemId=391989027483"
+   . "&maxResults=3"
+   . "&listingType=Chinese";
 
 
        $rest = simplexml_load_file($apicall) or die("Error: Please select the required filters");
@@ -17,7 +17,7 @@ $responseEncoding = 'XML';   // Format of the response
             $id=$item->itemId;
             echo "id ";
             echo $id;
-            echo </br>;
+            echo "</br>";
           }
 
 ?>
