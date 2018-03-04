d69862603b0924b4192af828952c84eee28ba1a4
Pius <piusjude@Pius.local>
2018-03-04 10:52:12 +0000

    new search page38

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ $responseEncoding = 'XML';   // Format of the response
 
 
   // Construct the FindItems call
-  $apicall = "http://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424&version=967&ItemId=273084369154';
+  $apicall = "http://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=PiusJude-Ragnarok-PRD-c5d80d3bd-40178424&version=967&ItemId=273084369154";
 
 
        $rest = simplexml_load_file($apicall) or die("Error: Please select the required filters");
