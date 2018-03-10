166da6ddd93d928dfead322a2a79626440dbf698
Pius <piusjude@Pius.local>
2018-03-10 16:55:31 +0000

    added extra column for product buttons10

--- products.php
+++ products.php
@@ -155,8 +155,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    document.getElementById(id).innerHTML = days + "days " + hours + "hours "
-    + minutes + "minutes " + seconds + "seconds ";
+    document.getElementById(id).innerHTML = days + " days " + hours + " hours "
+    + minutes + " minutes " + seconds + " seconds ";
 
     // If the count down is finished, write some text
     if (distance < 0) {
